d3ddd0d01fef934135e8fa232febcf14183bce11
muhammadedowardaya <muhammadedowarday4@gmail.com>
2025-03-30 09:57:00 +0700

    add url redis to redis module

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -2,17 +2,18 @@ import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { RedisController } from './redis.controller';
 import Redis from 'ioredis';
+import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
   providers: [
     {
       provide: 'REDIS_CLIENT',
+      inject: [ConfigService],
       useFactory: () => {
-        return new Redis({
-          host: 'localhost',
-          port: 6379,
-        });
+        return new Redis(
+          'rediss://default:Ab0PAAIjcDFhNWZlMWYzYWJiMjk0MjYzYjYzY2ZlOTExY2RjMDcyMHAxMA@fair-mullet-48399.upstash.io:6379',
+        );
       },
     },
     RedisService,
